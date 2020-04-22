7151707c9b06d3f8f0e12fe35d5448ab9c119195
dan <rzehak.d@gmail.com>
2020-04-22 12:47:39 +0200

    texture test I

--- js/planets.js
+++ js/planets.js
@@ -459,9 +459,11 @@ function PlanetDemo()
 				gl.texList[this.index] = gl.createTexture();
 				gl.bindTexture(gl.TEXTURE_2D, gl.texList[this.index]);
 				gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-				gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+				
+				gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this );
+				
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 				gl.generateMipmap(gl.TEXTURE_2D);
 				gl.bindTexture(gl.TEXTURE_2D, null);
 				++nTextures;
@@ -1297,7 +1299,7 @@ function PlanetDemo()
 
 		list[2] = new function () {
 			this.index = 3;
-			this.src = "img/night.jpg";
+			this.src = "";//"img/night.jpg";
 		};
 
 		list[3] = new function () {
